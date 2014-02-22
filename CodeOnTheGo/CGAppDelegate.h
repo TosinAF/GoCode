6ec9bc2b33683ed118c22779f985361bc5690216
Tosin Afolabi <psyooa@nottingham.ac.uk>
2014-02-22 15:12:19 +0000

    Initial Commit

---
+++ CodeOnTheGo/CGAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  CGAppDelegate.h
+//  CodeOnTheGo
+//
+//  Created by Tosin Afolabi on 22/02/2014.
+//  Copyright (c) 2014 Tosin Afolabi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CGAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ CodeOnTheGo/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  CodeOnTheGo
+//
+//  Created by Tosin Afolabi on 22/02/2014.
+//  Copyright (c) 2014 Tosin Afolabi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "CGAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([CGAppDelegate class]));
+    }
+}
